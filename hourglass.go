36ebfc70aaff17be76e85ae76fc97c09163a70b5
Andrey Sinitsyn <andrey.sinitsyn@lazada.com>
2016-11-02 19:28:28 +0700

    One fucking testcase

--- hourglass.go
+++ hourglass.go
@@ -13,13 +13,14 @@ func countHG(ar [][]string, x int, y int) int {
 	result := 0
 	var t, m int
 	if x >= 4 || y >= 4 {
+		fmt.Printf("Fast return: x = %d, y = %d", x, y)
 		return result
 	}
 	m = y
 	for i := 0; i <= 2; i++ { // horizontal dimension loop (X)
 		for j := 0; j <= 2; j++ { // vertical dimension loop (Y)
 			if (j == 0 && i == 1) || (j == 2 && i == 1) {
-				//				fmt.Printf("\t====> X: %d Y: %d not in operation\n", x, y)
+				fmt.Printf("\t====> X: %d Y: %d not in operation\n", x, y)
 				y++
 				if y == m+3 {
 					y = m
@@ -28,7 +29,7 @@ func countHG(ar [][]string, x int, y int) int {
 			}
 			t, _ = strconv.Atoi(ar[x][y])
 			result = result + t
-			//fmt.Printf("X: %d Y: %d M: %d VAL: %s\n", x, y, m, ar[x][y])
+			fmt.Printf("X: %d Y: %d M: %d VAL: %s\n", x, y, m, ar[x][y])
 			y++
 			if y == m+3 {
 				y = m
@@ -59,28 +60,28 @@ func main() {
 	//offsetX := 0
 	//offsetY := 0
 	curResult := 0
-	maxSum := 0 // current maximum hourglass value
+	maxSum := -100 // current maximum hourglass value
 	// curResult = countHG(finalAr, offsetX, offsetY)
 	// proceed glasses
-	for k := 0; k < 6; k++ {
-		for i := 0; i <= 5; i++ {
+	for k := 0; k <= 3; k++ {
+		for i := 0; i <= 3; i++ {
 			curResult = countHG(finalAr, k, i)
-			//			fmt.Printf("Cur result is %d Max sum id %d\n", curResult, maxSum)
-			//			fmt.Printf("==> I: %d J: %s\n", i, j)
+			fmt.Printf("===> Cur result is %d Max sum id %d\n", curResult, maxSum)
 			if curResult > maxSum {
 				maxSum = curResult
 			}
+			fmt.Printf("============================[ ITERATION COMPLETE; MAX RESULT IS %d ]=======================================\n\n", maxSum)
 		}
 	}
 	fmt.Printf("%d", maxSum)
 }
 
 /*
--1 -1 0 -9 -2 -2
--2 -1 -6 -8 -2 -5
--1 -1 -1 -2 -3 -4
--1 -9 -2 -4 -4 -5
--7 -3 -3 -2 -9 -9
--1 -3 -1 -2 -4 -5
-Incorrect input
+3 7 -3 0 1 8
+1 -4 -7 -8 -6 5
+-8 1 3 3 5 7
+-2 4 3 1 2 7
+2 4 -5 1 8 4
+5 -7 6 5 2 8
+Incorrect output (expected output 33)
 */
