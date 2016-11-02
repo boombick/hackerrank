722577e82ed11eabf035ac2d5265b6199d6010ce
Andrey Sinitsyn <andrey.sinitsyn@lazada.com>
2016-11-02 19:06:59 +0700

    Almost done

--- hourglass.go
+++ hourglass.go
@@ -16,13 +16,19 @@ func countHG(ar [][]string, x int, y int) int {
 		return result
 	}
 	m = y
-	for i := 0; i < 3; i++ { // vertical dimension loop
-		for j := 0; j < 3; j++ { // horizontal dimension loop
-			fmt.Printf("X: %d Y: %d M: %d VAL: %s\n", x, y, m, ar[x][y])
-			if j == 1 {
-				t, _ = strconv.Atoi(ar[x][y])
-				result = result + t
+	for i := 0; i <= 2; i++ { // horizontal dimension loop (X)
+		for j := 0; j <= 2; j++ { // vertical dimension loop (Y)
+			if (j == 0 && i == 1) || (j == 2 && i == 1) {
+				//				fmt.Printf("\t====> X: %d Y: %d not in operation\n", x, y)
+				y++
+				if y == m+3 {
+					y = m
+				}
+				continue
 			}
+			t, _ = strconv.Atoi(ar[x][y])
+			result = result + t
+			//fmt.Printf("X: %d Y: %d M: %d VAL: %s\n", x, y, m, ar[x][y])
 			y++
 			if y == m+3 {
 				y = m
@@ -30,6 +36,7 @@ func countHG(ar [][]string, x int, y int) int {
 		}
 		x++
 	}
+
 	return result
 }
 
@@ -41,10 +48,10 @@ func main() {
 	cnt := make([]byte, 256)
 	reader.Read(cnt)
 	arStr = string(cnt)
-	fmt.Println(arStr + "\n=======")
+	//	fmt.Println(arStr + "\n=======")
 	t1 := strings.Split(arStr, "\n")
 	for i := 0; i < 6; i++ {
-		fmt.Printf("Proceed string #%d: %s\n", i, t1[i])
+		//		fmt.Printf("Proceed string #%d: %s\n", i, t1[i])
 		finalAr[i] = strings.Split(t1[i], " ")
 	}
 
@@ -56,24 +63,24 @@ func main() {
 	// curResult = countHG(finalAr, offsetX, offsetY)
 	// proceed glasses
 	for k := 0; k < 6; k++ {
-		for i, j := range finalAr[k] {
+		for i := 0; i <= 5; i++ {
 			curResult = countHG(finalAr, k, i)
-			fmt.Printf("Cur result is %d Max sum id %d\n", curResult, maxSum)
-			fmt.Printf("==> I: %d J: %s\n", i, j)
+			//			fmt.Printf("Cur result is %d Max sum id %d\n", curResult, maxSum)
+			//			fmt.Printf("==> I: %d J: %s\n", i, j)
 			if curResult > maxSum {
 				maxSum = curResult
 			}
 		}
 	}
-	fmt.Printf("Result is: %d", maxSum)
+	fmt.Printf("%d", maxSum)
 }
 
 /*
-echo -ne "1 1 1 0 0 0
-0 1 0 0 0 0
-1 1 1 0 -9 0
-0 0 -1 0 0 0
-0 0 0 0 -2 0
-0 0 0 0 0 4
-" | ./bin/hourglass
+-1 -1 0 -9 -2 -2
+-2 -1 -6 -8 -2 -5
+-1 -1 -1 -2 -3 -4
+-1 -9 -2 -4 -4 -5
+-7 -3 -3 -2 -9 -9
+-1 -3 -1 -2 -4 -5
+Incorrect input
 */
