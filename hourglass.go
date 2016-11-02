12f2786a6461290b49cf913b5beb1f3a4c0b3b1f
Andrey Sinitsyn <andrey.sinitsyn@lazada.com>
2016-11-02 18:27:13 +0700

    Similar to right solution =)

--- hourglass.go
+++ hourglass.go
@@ -11,17 +11,20 @@ import (
 func countHG(ar [][]string, x int, y int) int {
 	// x must be less than 6 -2 -> must be less than 4
 	result := 0
-	var t int
+	var t, m int
 	if x >= 4 || y >= 4 {
 		return result
 	}
-
+	m = y
 	for i := 0; i < 3; i++ { // vertical dimension loop
 		for j := 0; j < 3; j++ { // horizontal dimension loop
 			fmt.Printf("X: %d Y: %d\n", x, y)
 			t, _ = strconv.Atoi(ar[x][y])
 			result = result + t
 			y++
+			if y == m+2 {
+				y = m
+			}
 		}
 		x++
 	}
@@ -50,10 +53,10 @@ func main() {
 	//maxSum := 0 // current maximum hourglass value
 	// curResult = countHG(finalAr, offsetX, offsetY)
 	// proceed glasses
-	for k, v := range finalAr {
-		for i, j := range v {
+	for k := 0; k < 6; k++ {
+		for i, j := range finalAr[k] {
 			curResult = countHG(finalAr, k, i)
-			fmt.Printf("I: %d J: %s\n", i, j)
+			fmt.Printf("==> I: %d J: %s\n", i, j)
 		}
 	}
 	fmt.Println(curResult)
