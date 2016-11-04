a09afe9466d1d21ebf690fb01c84dc513d9f00d8
Andrey Sinitsyn <andrey.sinitsyn@lazada.com>
2016-11-04 15:57:49 +0700

    Dynamics arrays continue

--- arrays/dar.go
+++ arrays/dar.go
@@ -1,15 +1,18 @@
-package dar
+package main
 
 import (
 	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-
+	isFirstLine := true
+	symbolCounter := 1 // it means first
+	var typeOfQuery int
 	for {
 
 		data := make([]byte, 1)
@@ -18,10 +21,35 @@ func main() {
 			break
 		}
 
-		if string(data) == " " {
-			continue
-		}
+		if isFirstLine { // first line logic
+			if string(data) == " " {
+				continue
+			} else if string(data) == "\n" {
+				isFirstLine = false
+				symbolCounter = 1
+			} else {
+				switch symbolCounter {
+				case 1:
+					countSeq, _ := strconv.Atoi(string(data))
+					seqStore := make([][]string, countSeq)
+				default:
+					continue
+				}
+				fmt.Printf("FIRST_LINE => %s\n", string(data))
+			}
+		} else { // other lines logic
+			if string(data) == " " {
+				continue
 
-		fmt.Printf("%s", string(data))
+			} else if string(data) == "\n" {
+				symbolCounter = 1
+			} else {
+				switch symbolCounter {
+				case 1: // type of query
+				}
+				fmt.Printf("%s", string(data))
+			}
+		}
+		symbolCounter++
 	}
 }
