ee99da1f4705031d1856e67cf3c99b7fd2b5ccdc
Andrey Sinitsyn <boombick@yandex.ru>
2016-11-21 18:25:26 +0700

    Array shift timeout case

--- arrays/left.go
+++ arrays/left.go
@@ -19,20 +19,21 @@ func removeIndex(s []string, index int) []string {
 	return append(s[:index], s[index+1:]...)
 }
 
-// Left shift arrays -- hackerrank
 func main() {
-	var s, d []string
+	var s string
 
 	reader := bufio.NewReader(os.Stdin)
 	firstLine, _ := reader.ReadString('\n')
 	firstLineSlice := strings.Split(trimSuffix(firstLine, "\n"), " ")
-	arrayLen, _ := strconv.Atoi(firstLineSlice[0])
 	countLoop, _ := strconv.Atoi(firstLineSlice[1])
 
 	secondLine, _ := reader.ReadString('\n')
 	secondLineSlice := strings.Split(trimSuffix(secondLine, "\n"), " ")
-	fmt.Printf("%#v\n%#v\n%#v", arrayLen, countLoop, secondLineSlice)
 
 	for i := 1; i <= countLoop; i++ {
+		s = secondLineSlice[0]
+		secondLineSlice = removeIndex(secondLineSlice, 0)
+		secondLineSlice = append(secondLineSlice, s)
 	}
+	fmt.Printf("%s\n", strings.Join(secondLineSlice, " "))
 }
