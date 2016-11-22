6864bb06370f4b31cdef4ce96cc44058bcfbb5dd
Andrey Sinitsyn <boombick@yandex.ru>
2016-11-22 14:48:12 +0700

    beautify

--- arrays/left.go
+++ arrays/left.go
@@ -28,7 +28,6 @@ func main() {
 
 	currentIndex := sliceSize - countLoop
 	finalSlice := make([]string, sliceSize)
-	fmt.Printf("count loop is %d\n", countLoop)
 	for i := 1; i <= sliceSize; i++ {
 		line, _ := reader.ReadString(' ')
 		line = strings.Trim(line, " \n")
